e213af8b91ce8a3c424748a6eda795929a467684
furkandlkdr <115067547+furkandlkdr@users.noreply.github.com>
2023-10-19 19:56:24 +0300

    Made first version of my linktree

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const shareButtonDOM = document.querySelectorAll('.tile-share-button');
+
+async function copyText(e) {
+    e.preventDefault();
+    const link = this.getAttribute('link');
+    try{
+        await navigator.clipboard.writeText(link);
+        alert('Link copied to clipboard' + link);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+shareButtonDOM.forEach((button) => {
+    button.addEventListener('click', copyText)
+});
